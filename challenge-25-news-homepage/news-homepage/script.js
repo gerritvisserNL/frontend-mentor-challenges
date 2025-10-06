33233d10564c730f422da977d3af9d3ede00d41f
GERRIT VISSER <gervisz@live.nl>
2025-10-06 20:42:24 +0200

    fix: add .menu-open to body with JS

--- challenge-25-news-homepage/news-homepage/script.js
+++ challenge-25-news-homepage/news-homepage/script.js
@@ -1,3 +1,4 @@
+const body = document.body;
 const menuOpen = document.querySelector(".menu-hamburger");
 const menuClose = document.querySelector(".menu-close");
 const menuMobile = document.querySelector(".menu-mobile");
@@ -6,9 +7,11 @@ const overlay = document.querySelector(".overlay");
 menuOpen.addEventListener("click", () => {
   overlay.classList.add("active");
   menuMobile.classList.add("active");
+  body.classList.add("menu-open");
 });
 
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("active");
   menuMobile.classList.remove("active");
+  body.classList.remove("menu-open");
 });
