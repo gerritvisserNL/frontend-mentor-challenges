8bb4a56eafe93f25ab2d4ca5723e98299e93f4b2
GERRIT VISSER <gervisz@live.nl>
2025-10-08 14:08:33 +0200

    Refactor: simplify JavaScript code for clarity

--- challenge-26-sunnyside-agency-landing-page/sunnyside-agency-landing-page/script.js
+++ challenge-26-sunnyside-agency-landing-page/sunnyside-agency-landing-page/script.js
@@ -1,10 +1,7 @@
-const body = document.body;
 const menuIcon = document.querySelector(".hamburger-menu");
 const menuMobile = document.querySelector(".menu-mobile");
 
 menuIcon.addEventListener("click", (event) => {
-  event.stopPropagation();
-
   if (menuMobile.classList.contains("hidden")) {
     menuMobile.classList.remove("hidden");
   } else {
@@ -12,10 +9,7 @@ menuIcon.addEventListener("click", (event) => {
   }
 });
 
-body.addEventListener("click", () => {
-  if (!menuMobile.classList.contains("hidden")) {
-    menuMobile.classList.add("hidden");
-  } else {
-    return;
-  }
-});
+// alternative code
+// menuIcon.addEventListener("click", () => {
+//   menuMobile.classList.toggle("hidden");
+// });
