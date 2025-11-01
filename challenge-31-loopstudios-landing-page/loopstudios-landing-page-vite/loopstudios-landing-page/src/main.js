3202647e07fb1a04bd2dc043dd08f9a161b90827
GERRIT VISSER <gervisz@live.nl>
2025-11-01 16:10:26 +0100

    feat: complete html, mobile-first layout, javascript

--- challenge-31-loopstudios-landing-page/loopstudios-landing-page-vite/loopstudios-landing-page/src/main.js
+++ challenge-31-loopstudios-landing-page/loopstudios-landing-page-vite/loopstudios-landing-page/src/main.js
@@ -1,2 +1,14 @@
 import "./reset.css";
 import "./style.css";
+
+const openBtn = document.querySelector(".header__toggle-icon");
+const closeBtn = document.querySelector(".header__nav-close");
+const menu = document.querySelector(".header__nav-mobile");
+
+openBtn.addEventListener("click", () => {
+  menu.classList.add("header__nav-mobile--active");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("header__nav-mobile--active");
+});
